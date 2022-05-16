b2eabb6989accab133a8068f8967caa8605c0808
kyeager <kyeager@ewu.edu>
2022-05-16 14:13:00 -0700

    update API key
    in js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*** Variables ***/
 // API Key
-const API_KEY = 'vRgpxNRNKxyIlc1sk9lEifd6kDJZDK57';
+const API_KEY = '5mUBZNSPHT1CDqipWwfRMAlWRgwim6gr';
 // limit
 const limit = 25; 
 // Get `input`
